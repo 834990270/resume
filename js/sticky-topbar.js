d3ff5536f40abf5fa22ec3028e6427bfdddc18b0
gaoxuewei <834990270@qq.com>
2019-01-21 12:43:44 +0800

    smoothly-navigation.js失效

--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -1,9 +1,27 @@
 !function () {
-    window.addEventListener('scroll', function (x) {
-        if (window.scrollY > 0) {
-            topNavBar.classList.add('sticky')
-        } else {
-            topNavBar.classList.remove('sticky')
+    var view = document.querySelector('#topNavBar')
+    var controller={
+        view:null,
+        init:function(view){
+            this.view=view
+            this.bindEvents()
+        },
+        bindEvents:function(){
+            var view=this.view
+            window.addEventListener('scroll', (x)=>{
+                if (window.scrollY > 0) {
+                    this.active()
+                } else {
+                    this.decative()
+                }
+            })
+        },
+        active:function(){
+            this.view.classList.add('sticky')
+        },
+        decative:function(){
+            this.view.classList.remove('sticky')
         }
-    })
+    }
+    controller.init(view)
 }.call()
